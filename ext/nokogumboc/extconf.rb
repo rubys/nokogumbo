9b6d08bcc7fa14afb22fdb34877f357211ee0214
Steve Wills <steve@mouf.net>
2015-07-25 23:21:28 -0400

    Use better way to find extension dir
    This way is easier to understand and returns a more correct directory.

--- ext/nokogumboc/extconf.rb
+++ ext/nokogumboc/extconf.rb
@@ -10,7 +10,7 @@ if have_library('xml2', 'xmlNewDoc')
     select { |name| name.match(%r{gems/nokogiri-([\d.]+)/lib/nokogiri}) }.
     sort_by {|name| name[/nokogiri-([\d.]+)/,1].split('.').map(&:to_i)}.last
   if nokogiri_lib
-    nokogiri_ext = nokogiri_lib.sub(%r(lib/nokogiri(.rb)?$), 'ext/nokogiri')
+    nokogiri_ext = Gem::Specification.find_by_name('nokogiri').extension_dir
 
     # if that doesn't work, try workarounds found in Nokogiri's extconf
     unless find_header('nokogiri.h', nokogiri_ext)
